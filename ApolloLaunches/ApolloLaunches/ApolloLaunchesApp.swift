ac0ab40dab22d9759c9eafe59eb498a4231844a3
Sabelo N <sabeloxnxele@gmail.com>
2024-07-10 20:43:17 +0200

    initial commit

---
+++ ApolloLaunches/ApolloLaunches/ApolloLaunchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloLaunchesApp.swift
+//  ApolloLaunches
+//
+//  Created by Tu Sabe Oeste on 2024/07/10.
+//
+
+import SwiftUI
+
+@main
+struct ApolloLaunchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
