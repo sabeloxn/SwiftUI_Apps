dfdfe844016fc4ccd37704fce700c84b5521c9f4
Sabelo N <sabeloxnxele@gmail.com>
2024-07-11 21:18:35 +0200

    data retrieved

---
+++ ApolloPlanets/ApolloPlanets/ApolloPlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloPlanetsApp.swift
+//  ApolloPlanets
+//
+//  Created by Tu Sabe Oeste on 2024/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ApolloPlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApolloPlanets/ApolloPlanets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApolloPlanets
+//
+//  Created by Tu Sabe Oeste on 2024/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
