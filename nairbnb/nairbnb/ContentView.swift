029597a9638de6aff2a2aa37142d8cd6e173ba02
Sabelo N <sabeloxnxele@gmail.com>
2024-08-13 15:15:25 +0200

    initial commit

---
+++ nairbnb/nairbnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nairbnb
+//
+//  Created by Tu Sabe Oeste on 2024/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
