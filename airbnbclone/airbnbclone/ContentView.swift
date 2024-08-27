4b5f50ec04c88bc9e949a5cbd39afae83023ffa4
Sabelo N <sabeloxnxele@gmail.com>
2024-08-27 21:30:52 +0200

    initial commit

---
+++ airbnbclone/airbnbclone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airbnbclone
+//
+//  Created by Tu Sabe Oeste on 2024/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airbnbclone/airbnbclone/airbnbcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbcloneApp.swift
+//  airbnbclone
+//
+//  Created by Tu Sabe Oeste on 2024/08/27.
+//
+
+import SwiftUI
+
+@main
+struct airbnbcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
